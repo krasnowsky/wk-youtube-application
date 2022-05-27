5068cd2ac8467dae890d26e673129d96a16ccd00
krasnowsky <wojtek.ref2@gmail.com>
2022-05-28 00:40:31 +0200

    Turned get_data into class

--- get_data.py
+++ get_data.py
@@ -5,65 +5,67 @@ import config_parser
 import requests
 import os
 
-API_KEY = config_parser.api_key
+class Data:
 
-channel_names = ['ekipa_wk', 'wk_dzik_pl', 'warszawski_koks', 'kuchnia_wk', 'wk_gaming']
-channel_ids = ['UCnvrd6z-UgyX0n-Db7sQI4Q', 'UCUr1w6sHtgj1JniKV8vWXMw', 'UC2AyohFiDUS3K98h5dJVfog', 'UC4TYJ_RcqwL9lAZgkQlk11g', 'UCeLWHfuhwnObampm0M6oH4w']
+	def __init__(self):
+		self.API_KEY = config_parser.api_key
 
-#			0		1			2				3		4
-#position - id, video_url, thumbnail_url, timestamp, title
-def read_data(video_number, position, channel_name):
-	data_path = f'/Users/krasnowsky/wk_youtube/data/{channel_name}/videos.data'
-	with open(data_path) as f:
-		lines = f.readlines()
-		return lines[video_number].split(';')[position]
+		self.channel_names = ['ekipa_wk', 'wk_dzik_pl', 'warszawski_koks', 'kuchnia_wk', 'wk_gaming']
+		self.channel_ids = ['UCnvrd6z-UgyX0n-Db7sQI4Q', 'UCUr1w6sHtgj1JniKV8vWXMw', 'UC2AyohFiDUS3K98h5dJVfog', 'UC4TYJ_RcqwL9lAZgkQlk11g', 'UCeLWHfuhwnObampm0M6oH4w']
 
-def download_thumbnail(url, video_id, channel_name):
-	response = requests.get(url)
-	path = f'/Users/krasnowsky/wk_youtube/data/{channel_name}/thumbnails/{video_id}.png'
-	file = open(path, 'wb')
-	file.write(response.content)
-	file.close()
+	#			0		1			2				3		4
+	#position - id, video_url, thumbnail_url, timestamp, title
+	def read_data(video_number, position, channel_name):
+		data_path = f'/Users/krasnowsky/wk_youtube/data/{channel_name}/videos.data'
+		with open(data_path) as f:
+			lines = f.readlines()
+			return lines[video_number].split(';')[position]
 
-def write_data(video, channel_name, flag):
-	path = f'/Users/krasnowsky/wk_youtube/data/{channel_name}/videos.data'
-	line = f'{video.id};{video.url};{video.thumbnail};{video.published_at};{video.title}\n'
-	if flag:
-		file = open(path, "a")
-		file.write(line)
+	def download_thumbnail(url, video_id, channel_name):
+		response = requests.get(url)
+		path = f'/Users/krasnowsky/wk_youtube/data/{channel_name}/thumbnails/{video_id}.png'
+		file = open(path, 'wb')
+		file.write(response.content)
 		file.close()
-	else:
-		with open(path, 'r+') as file:
-			content = file.read()
-			file.seek(0)
-			file.write(line + content)
 
-def get_lines_amount(channel_name):
-	with open(f'/Users/krasnowsky/wk_youtube/data/{channel_name}/videos.data', 'r') as fp:
-		x = len(fp.readlines())
-		return x
+	def write_data(video, channel_name, flag):
+		path = f'/Users/krasnowsky/wk_youtube/data/{channel_name}/videos.data'
+		line = f'{video.id};{video.url};{video.thumbnail};{video.published_at};{video.title}\n'
+		if flag:
+			file = open(path, "a")
+			file.write(line)
+			file.close()
+		else:
+			with open(path, 'r+') as file:
+				content = file.read()
+				file.seek(0)
+				file.write(line + content)
 
-def get_data():
-	for i in range(5):
-		data = youtube_api(API_KEY, channel_ids[i], 1)
-		data.get_channel_video_data()
+	def get_lines_amount(channel_name):
+		with open(f'/Users/krasnowsky/wk_youtube/data/{channel_name}/videos.data', 'r') as fp:
+			x = len(fp.readlines())
+			return x
 
-		if_file_empty = True
-		videos_in_file = get_lines_amount(channel_names[i])
-		if videos_in_file != 0:
-			if_file_empty = False
+	def get_data(self):
+		for i in range(5):
+			data = youtube_api(self.API_KEY, self.channel_ids[i], 1)
+			data.get_channel_video_data()
 
-		videos_ids = []
+			if_file_empty = True
+			videos_in_file = self.get_lines_amount(self.channel_names[i])
+			if videos_in_file != 0:
+				if_file_empty = False
 
-		for line_number in range(videos_in_file):
-			videos_ids[line_number] = read_data(line_number, 0, channel_names[i])
+			videos_ids = []
 
-		for vid in data.videos:
-			if vid.id not in videos_ids:
-				download_thumbnail(vid.thumbnail, vid.id, channel_names[i])
-				write_data(vid, channel_names[i], if_file_empty)
+			for line_number in range(videos_in_file):
+				videos_ids[line_number] = self.read_data(line_number, 0, self.channel_names[i])
 
-get_data()
+			for vid in data.videos:
+				if vid.id not in videos_ids:
+					self.download_thumbnail(vid.thumbnail, vid.id, self.channel_names[i])
+					self.write_data(vid, self.channel_names[i], if_file_empty)
+
 
 
 
